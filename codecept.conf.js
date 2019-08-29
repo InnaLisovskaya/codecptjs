cf4af93f93250db89d4d80e0af10b90b0721da7a
Inna <innalis1985@mail.ru>
2019-08-29 09:09:24 +0500

    Added mocha reporters

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,12 +1,25 @@
 exports.config = {
   tests: './tests/*_test.js',
   output: './output',
+
+
   helpers: {
     WebDriver: {
       url: 'http://localhost',
-      browser: 'chrome'
+      browser: 'chrome',
+      waitForTimeout: 10000,
+      desiredCapabilities: {
+        chromeOptions: {
+          args: [ '--headless','--disable-gpu', '--window-size=2048,1080'],
+         //   args: ['--disable-gpu', '--window-size=1280,1200'],
+        },
+      },
+    },
+    Mochawesome: {
+      uniqueScreenshotNames: "true"
     }
   },
+
   plugins: {
     wdio: {
       enabled: true,
@@ -17,7 +30,12 @@ exports.config = {
     I: './steps_file.js'
   },
   bootstrap: null,
-  mocha: {},
+  mocha: {
+    reporterOptions: {
+      mochaFile: "output/result.xml",
+      reportDir: "output"
+    }
+  },
   name: 'csssr'
 }
 
